5b7c040a9d4f2b4865d13f9ec7ec17e828b96c1d
nafiz-hossain <nhremon8181@gmail.com>
2024-05-04 06:58:37 +0600

    First batch run test cases

--- cypress/e2e/github_link_test.cy.ts
+++ cypress/e2e/github_link_test.cy.ts
@@ -1,16 +1,17 @@
-describe('Github link test', ()=>{
+describe('Github link test', () => {
     beforeEach(() => {
-        cy.visit('http://nafiz-hossain.github.io/portfolio')
-    })
-    it('Github button availability', ()=>{
-        cy.get('[class="sc-gsFSXq bSXTty"]')
-    })
-
-    it('Clicks on the "Github Profile" link', () => {
-        cy.get('div.sc-kAyceB.bKssKc')
-          .find('a.sc-gsFSXq.bSXTty') 
-          .click();    
-          cy.url().should('include', 'github.com/nafiz-hossain')
-                 
+        cy.visit('http://nafiz-hossain.github.io/portfolio');
     });
-})
+
+    it('should check availability of Github button', () => {
+        // Assert that the Github button is present
+        cy.get('[class="sc-gsFSXq bSXTty"]').should('exist');
+    });
+
+    it('Check redirection', () => {
+        cy.visit('http://nafiz-hossain.github.io/portfolio');
+        
+        // Check if the link with the specified text content exists and has the correct href attribute
+        cy.contains('Github Profile').should('have.attr', 'href', 'https://github.com/nafiz-hossain');
+    })
+});
