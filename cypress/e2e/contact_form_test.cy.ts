9b50d505ac2bd3855088f9b88d369d0d2c098752
nafiz-hossain <nhremon8181@gmail.com>
2024-05-04 07:31:00 +0600

    Fixed bug in contact form test

--- cypress/e2e/contact_form_test.cy.ts
+++ cypress/e2e/contact_form_test.cy.ts
@@ -2,6 +2,16 @@ describe('Check contact form', () => {
     beforeEach(() => {
         cy.visit('http://nafiz-hossain.github.io/portfolio')
     })
+    it('Contact form test with invalid mail', () => {
+        cy.get('[name="from_email"]').type('test_mail')
+        cy.get('[name="subject"]').type('Test subject')
+        cy.get('[name="message"]').type('This is a test message')
+        cy.get('[name="from_name"]').type('Test name')
+        cy.get('[type="submit"]').click()
+        cy.contains('Please enter a valid email address.').should('be.visible')
+
+    })
+    
     it('Contact form test with valid data', () => {
         cy.get('[name="from_email"]').type('test_mail@gmail.com')
         cy.get('[name="subject"]').type('Test subject')
@@ -12,13 +22,5 @@ describe('Check contact form', () => {
 
     })
 
-    it('Contact form test with invalid mail', () => {
-        cy.get('[name="from_email"]').type('test_mail')
-        cy.get('[name="subject"]').type('Test subject')
-        cy.get('[name="message"]').type('This is a test message')
-        cy.get('[name="from_name"]').type('Test name')
-        cy.get('[type="submit"]').click()
-        cy.contains('Please enter a valid email address.').should('be.visible')
 
-    })
 })
